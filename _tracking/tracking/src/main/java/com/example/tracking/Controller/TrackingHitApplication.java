381c4ba5274449ead954f361d4d6acce76e810d5
mii2026 <mii2026@skku.edu>
2023-10-15 15:56:45 +0900

    add swagger setting

--- _tracking/tracking/src/main/java/com/example/tracking/Controller/TrackingHitApplication.java
+++ _tracking/tracking/src/main/java/com/example/tracking/Controller/TrackingHitApplication.java
@@ -3,6 +3,8 @@ package com.example.tracking.Controller;
 import com.example.tracking.DTO.HistoryDTO;
 import com.example.tracking.DTO.HitsDTO;
 import com.example.tracking.Service.TrackingHitService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,8 +20,10 @@ import java.util.Map;
 public class TrackingHitApplication {
     private final TrackingHitService trackingHitService;
 
+    @Operation(summary = "url 조회수 증가", description = "해당 url이 존재하면 일일 조회수와 총 조회수를 증가하고, 아니면 url 조회수 정보를 생성합니다.")
     @PutMapping("/hits")
-    public ResponseEntity<Object> addHits(@RequestParam String url){
+    public ResponseEntity<Object> addHits(@Parameter(name = "url", description = "url을 작성합니다.(':'는 '%3A', '/'는 '%2F'로 작성)")
+                                              @RequestParam String url){
         this.trackingHitService.addHits(url);
 
         Map<String, String> response = new HashMap<>(){};
@@ -27,14 +31,18 @@ public class TrackingHitApplication {
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(summary = "url 조회수 조회", description = "해당 url의 일일 조회수와 총 조회수를 조회합니다.")
     @GetMapping("/hits")
-    public ResponseEntity<Object> getHits(@RequestParam String url){
+    public ResponseEntity<Object> getHits(@Parameter(name = "url", description = "url을 작성합니다.(':'는 '%3A', '/'는 '%2F'로 작성)")
+                                              @RequestParam String url){
         HitsDTO info = this.trackingHitService.getHits(url);
         return ResponseEntity.ok().body(info);
     }
 
+    @Operation(summary = "url 히스토리 조회", description = "해당 url의 7일간의 조회수를 조회합니다.")
     @GetMapping("/history")
-    public ResponseEntity<Object> getHistory(@RequestParam String url){
+    public ResponseEntity<Object> getHistory(@Parameter(name = "url", description = "url을 작성합니다.(':'는 '%3A', '/'는 '%2F'로 작성)")
+                                                 @RequestParam String url){
         HistoryDTO info = this.trackingHitService.getHistory(url);
         return ResponseEntity.ok().body(info);
     }
