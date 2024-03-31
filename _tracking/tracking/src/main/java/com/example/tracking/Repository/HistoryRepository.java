0f3c05f5057309d4381b23eb5a05fa32541a066f
mii2026 <mii2026@skku.edu>
2024-04-01 04:49:15 +0900

    [REFACT] 레파지토리 코드 정리(안쓰는 코드 삭제)

--- _tracking/tracking/src/main/java/com/example/tracking/Repository/HistoryRepository.java
+++ _tracking/tracking/src/main/java/com/example/tracking/Repository/HistoryRepository.java
@@ -1,13 +1,7 @@
 package com.example.tracking.Repository;
 
-import com.example.tracking.Entity.Daily;
 import com.example.tracking.Entity.History;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
 }
